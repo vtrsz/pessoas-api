eb67ebb8ae23026a6deb548808cfd000ffba286e
vtrsz <vitorsouzaribeiro1@gmail.com>
2023-01-26 16:55:20 -0300

    feat(swagger): info added

--- src/main/java/com/attornatus/attornatus/PessoasApiApplication.java
+++ src/main/java/com/attornatus/attornatus/PessoasApiApplication.java
@@ -1,13 +1,14 @@
 package com.attornatus.attornatus;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Pessoas API", version = "1.0", description = "Desafio de Código proposto pela Attornatus"))
 public class PessoasApiApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(PessoasApiApplication.class, args);
     }
-
 }
