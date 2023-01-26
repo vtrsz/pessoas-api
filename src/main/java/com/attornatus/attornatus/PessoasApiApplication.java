f664108537a0121300137bd0011e13c828522ee9
vtrsz <vitorsouzaribeiro1@gmail.com>
2023-01-26 16:47:37 -0300

    Spring base project

---
+++ src/main/java/com/attornatus/attornatus/PessoasApiApplication.java
@@ -0,0 +1,13 @@
+package com.attornatus.attornatus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PessoasApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PessoasApiApplication.class, args);
+    }
+
+}
