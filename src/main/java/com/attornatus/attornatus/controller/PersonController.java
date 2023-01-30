41c428b8b3f5a43771a01049745a377fd9be6128
vtrsz <vitorsouzaribeiro1@gmail.com>
2023-01-29 21:08:11 -0300

    feat(controller): person implemented

---
+++ src/main/java/com/attornatus/attornatus/controller/PersonController.java
@@ -0,0 +1,10 @@
+package com.attornatus.attornatus.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/api/person", produces = {"application/json"}, consumes = {"application/json"})
+public class PersonController {
+    
+}
