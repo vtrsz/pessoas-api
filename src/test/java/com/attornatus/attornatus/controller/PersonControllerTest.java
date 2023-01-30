e9c5b3277680729f326dbdf9972c47222d66fb11
vtrsz <vitorsouzaribeiro1@gmail.com>
2023-01-29 22:17:18 -0300

    feat(test): should return 201 when create new person

--- src/test/java/com/attornatus/attornatus/controller/PersonControllerTest.java
+++ src/test/java/com/attornatus/attornatus/controller/PersonControllerTest.java
@@ -1,11 +1,58 @@
 package com.attornatus.attornatus.controller;
 
+import com.attornatus.attornatus.dto.AddressAttachedPersonDTO;
+import com.attornatus.attornatus.dto.CreateAddressDTO;
+import com.attornatus.attornatus.dto.CreatePersonDTO;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(controllers = PersonController.class)
 @ExtendWith(SpringExtension.class)
 public class PersonControllerTest {
+    @Autowired
+    private MockMvc mvc;
 
+    DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+    public static String asJsonString(final Object obj) {
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            objectMapper.registerModule(new JavaTimeModule());
+            return objectMapper.writeValueAsString(obj);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    public void shouldReturn201WhenCreateNewPerson() throws Exception {
+        List<AddressAttachedPersonDTO> addresses = Arrays.asList(
+                new AddressAttachedPersonDTO("Rua da Escola", "1502", "São Paulo", "SP", "00000000", true),
+                new AddressAttachedPersonDTO("Rua da Lagoa", "10", "Recife", "PE", "00000000", false)
+        );
+
+        mvc.perform(MockMvcRequestBuilders
+                        .post("/api/person")
+                        .content(asJsonString(new CreatePersonDTO("John Doe", LocalDate.parse("01/01/2000", dateFormatter), addresses)))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id").exists());
+    }
 }
