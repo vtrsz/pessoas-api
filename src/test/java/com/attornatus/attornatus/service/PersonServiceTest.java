567136b8f5c4aca91584f19d43de8eb7d9c29762
vtrsz <vitorsouzaribeiro1@gmail.com>
2023-01-31 22:00:19 -0300

    feat(test): service update person

--- src/test/java/com/attornatus/attornatus/service/PersonServiceTest.java
+++ src/test/java/com/attornatus/attornatus/service/PersonServiceTest.java
@@ -2,6 +2,7 @@ package com.attornatus.attornatus.service;
 
 import com.attornatus.attornatus.dto.request.AddressAttachedPersonDTO;
 import com.attornatus.attornatus.dto.request.CreatePersonDTO;
+import com.attornatus.attornatus.entity.Address;
 import com.attornatus.attornatus.entity.Person;
 import com.attornatus.attornatus.exception.BusinessRuleException;
 import com.attornatus.attornatus.exception.MultipleMainAddressException;
@@ -13,12 +14,12 @@ import org.mockito.Mockito;
 import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 
 public class PersonServiceTest {
@@ -40,6 +41,12 @@ public class PersonServiceTest {
                 .name("John Doe")
                 .birthDate(LocalDate.parse("2000-01-01"))
                 .addresses(addressesDTO).build();
+
+        Person person = new Person(1L, "John Doe", LocalDate.parse("2000-01-01"), List.of(
+        ));
+        person.setAddresses(List.of(new Address(1L, "Rua da Lagoa", "10", "Recife", "PE", "0000000", true, person)));
+
+        when(personRepository.save(any(Person.class))).thenReturn(person);
 
         personService.createPerson(createPersonDTO);
 
@@ -101,6 +108,10 @@ public class PersonServiceTest {
 
     @Test
     public void shouldGetAPerson() {
+        Person person = new Person(1L, "John Doe", LocalDate.parse("2000-01-01"), List.of());
+        person.setAddresses(List.of(new Address(1L, "Rua da Lagoa", "10", "Recife", "PE", "0000000", true, person)));
+
+        when(personRepository.findById(1L)).thenReturn(Optional.of(person));
         personService.getPersonById(1L);
 
         verify(personRepository, times(1)).findById(1L);
@@ -108,7 +119,109 @@ public class PersonServiceTest {
 
     @Test
     public void shouldNotFoundWhenGetAPerson() {
-        assertTrue(personService.getPersonById(1L).isEmpty());
+        assertEquals(personService.getPersonById(1L), Optional.empty());
         verify(personRepository, times(1)).findById(1L);
     }
+
+    @Test
+    public void shouldUpdateAPerson() throws MultipleMainAddressException, BusinessRuleException {
+        List<AddressAttachedPersonDTO> addressesDTO = Arrays.asList(
+                new AddressAttachedPersonDTO("Rua da Escola", "1502", "São Paulo", "SP", "00000000", true),
+                new AddressAttachedPersonDTO("Rua da Lagoa", "10", "Recife", "PE", "00000001", false));
+        CreatePersonDTO createPersonDTO = CreatePersonDTO.builder()
+                .name("John Doe")
+                .birthDate(LocalDate.parse("2000-01-01"))
+                .addresses(addressesDTO).build();
+
+        Person person = new Person(1L, "John Doe", LocalDate.parse("2000-01-01"), List.of());
+        person.setAddresses(List.of(new Address(1L, "Rua da Lagoa", "10", "Recife", "PE", "0000000", true, person)));
+
+        when(personRepository.findById(1L)).thenReturn(Optional.of(person));
+        when(personRepository.save(any(Person.class))).thenReturn(person);
+
+        personService.updatePersonById(createPersonDTO, 1L);
+
+        verify(personRepository, times(1)).save(any(Person.class));
+    }
+
+    @Test
+    public void shouldNotFoundWhenUpdateAPerson() throws MultipleMainAddressException, BusinessRuleException {
+        when(personRepository.findById(1L)).thenReturn(Optional.empty());
+
+        assertEquals(personService.updatePersonById(new CreatePersonDTO(), 1L), Optional.empty());
+        verify(personRepository, times(1)).findById(1L);
+        verify(personRepository, times(0)).save(any(Person.class));
+    }
+
+    @Test
+    public void shouldThrowIllegalArgumentExceptionWhenUpdateAPerson() {
+        when(personRepository.findById(1L)).thenReturn(Optional.of(new Person()));
+
+        assertThrows(IllegalArgumentException.class, () -> personService.updatePersonById(null, 1L));
+
+        verify(personRepository, times(1)).findById(1L);
+        verify(personRepository, times(0)).save(any(Person.class));
+    }
+
+    @Test
+    public void shouldThrowBusinessRuleExceptionWhenUpdateAPersonWithNullAddress() {
+        CreatePersonDTO personDTO = CreatePersonDTO.builder()
+            .name("John Doe")
+            .birthDate(LocalDate.parse("2000-01-01")).build();
+        when(personRepository.findById(1L)).thenReturn(Optional.of(new Person()));
+
+        assertThrows(BusinessRuleException.class, () -> personService.updatePersonById(personDTO, 1L));
+
+        verify(personRepository, times(1)).findById(1L);
+        verify(personRepository, times(0)).save(any(Person.class));
+    }
+
+    @Test
+    public void shouldThrowBusinessRuleExceptionWhenUpdateAPersonWithEmptyAddress() {
+        CreatePersonDTO personDTO = CreatePersonDTO.builder()
+                .name("John Doe")
+                .birthDate(LocalDate.parse("2000-01-01"))
+                .addresses(List.of()).build();
+        when(personRepository.findById(1L)).thenReturn(Optional.of(new Person()));
+
+        assertThrows(BusinessRuleException.class, () -> personService.updatePersonById(personDTO, 1L));
+
+        verify(personRepository, times(1)).findById(1L);
+        verify(personRepository, times(0)).save(any(Person.class));
+    }
+
+    @Test
+    public void shouldThrowMultipleMainAddressExceptionWhenUpdateAPersonWithMultipleMainAddress() {
+        List<AddressAttachedPersonDTO> addressesDTO = Arrays.asList(
+                new AddressAttachedPersonDTO("Rua da Escola", "1502", "São Paulo", "SP", "00000000", true),
+                new AddressAttachedPersonDTO("Rua da Lagoa", "10", "Recife", "PE", "00000001", true));
+        CreatePersonDTO personDTO = CreatePersonDTO.builder()
+                .name("John Doe")
+                .birthDate(LocalDate.parse("2000-01-01"))
+                .addresses(addressesDTO).build();
+        when(personRepository.findById(1L)).thenReturn(Optional.of(new Person()));
+
+        assertThrows(MultipleMainAddressException.class, () -> personService.updatePersonById(personDTO, 1L));
+
+        verify(personRepository, times(1)).findById(1L);
+        verify(personRepository, times(0)).save(any(Person.class));
+    }
+
+    @Test
+    public void shouldThrowBusinessRuleExceptionWhenUpdateAPersonWithoutMainAddress() {
+        List<AddressAttachedPersonDTO> addressesDTO = Arrays.asList(
+                new AddressAttachedPersonDTO("Rua da Escola", "1502", "São Paulo", "SP", "00000000", false),
+                new AddressAttachedPersonDTO("Rua da Lagoa", "10", "Recife", "PE", "00000001", false));
+        CreatePersonDTO personDTO = CreatePersonDTO.builder()
+                .name("John Doe")
+                .birthDate(LocalDate.parse("2000-01-01"))
+                .addresses(addressesDTO).build();
+        when(personRepository.findById(1L)).thenReturn(Optional.of(new Person()));
+
+        assertThrows(BusinessRuleException.class, () -> personService.updatePersonById(personDTO, 1L));
+
+        verify(personRepository, times(1)).findById(1L);
+        verify(personRepository, times(0)).save(any(Person.class));
+    }
+
 }
