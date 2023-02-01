d7e1927688b86c2ff127eeec14bdbfedeb4ded83
vtrsz <vitorsouzaribeiro1@gmail.com>
2023-02-01 20:48:15 -0300

    feat(test): controller create new adress

---
+++ src/test/java/com/attornatus/attornatus/controller/AddressControllerTest.java
@@ -0,0 +1,109 @@
+package com.attornatus.attornatus.controller;
+
+import com.attornatus.attornatus.dto.request.CreateAddressDTO;
+import com.attornatus.attornatus.dto.response.ResponseAddressDTO;
+import com.attornatus.attornatus.exception.BusinessRuleException;
+import com.attornatus.attornatus.service.AddressService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(controllers = AddressController.class)
+public class AddressControllerTest {
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    private AddressService addressService;
+
+    public static String asJsonString(final Object obj) {
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            objectMapper.registerModule(new JavaTimeModule());
+            return objectMapper.writeValueAsString(obj);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    public void shouldReturn201WhenCreateNewAddress() throws Exception {
+        CreateAddressDTO createAddressDTO = CreateAddressDTO.builder().street("Rua da Igreja")
+                .number("12")
+                .city("São Paulo")
+                .state("SP")
+                .cep("00000000")
+                .main(true)
+                .personId(1L).build();
+
+        ResponseAddressDTO responseAddressDTO = ResponseAddressDTO.builder().id(1L)
+                .street("Rua da Igreja")
+                .number("12")
+                .city("São Paulo")
+                .state("SP")
+                .cep("00000000")
+                .main(true)
+                .personId(1L).build();
+
+        given(addressService.createAddress(any(CreateAddressDTO.class))).willAnswer((invocation) -> responseAddressDTO);
+
+        mvc.perform(post("/api/address")
+                        .content(asJsonString(createAddressDTO))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id").value(responseAddressDTO.getId()))
+                .andExpect(jsonPath("$.street").value(responseAddressDTO.getStreet()))
+                .andExpect(jsonPath("$.number").value(responseAddressDTO.getNumber()))
+                .andExpect(jsonPath("$.city").value(responseAddressDTO.getCity()))
+                .andExpect(jsonPath("$.state").value(responseAddressDTO.getState()))
+                .andExpect(jsonPath("$.cep").value(responseAddressDTO.getCep()))
+                .andExpect(jsonPath("$.main").value(responseAddressDTO.getMain()))
+                .andExpect(jsonPath("$.personId").value(responseAddressDTO.getPersonId()));
+    }
+
+    @Test
+    public void shouldReturn400WhenCreateNewAddress() throws Exception {
+        mvc.perform(post("/api/address")
+                        .content(asJsonString(null))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.errors").exists());
+    }
+
+    @Test
+    public void shouldReturn422WhenCreateNewAddress() throws Exception {
+        CreateAddressDTO createAddressDTO = CreateAddressDTO.builder().street("Rua da Igreja")
+                .number("12")
+                .city("São Paulo")
+                .state("SP")
+                .cep("00000000")
+                .main(true)
+                .personId(0L).build();
+
+        given(addressService.createAddress(any(CreateAddressDTO.class))).willThrow(new BusinessRuleException("a person with this personId is not found"));
+
+        mvc.perform(post("/api/address")
+                        .content(asJsonString(createAddressDTO))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isUnprocessableEntity())
+                .andExpect(jsonPath("$.errors").exists());
+    }
+}
